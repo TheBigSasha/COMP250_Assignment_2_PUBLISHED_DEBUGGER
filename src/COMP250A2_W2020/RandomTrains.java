4a6932ec58f8cc8060f8f7488fb062209c704f4b
thebigsasha <sashathethird@gmail.com>
2020-02-23 16:52:04 -0500

    Initial commit

---
+++ src/COMP250A2_W2020/RandomTrains.java
@@ -0,0 +1,90 @@
+package COMP250A2_W2020;
+
+import java.util.Random;
+
+public class RandomTrains extends Random {
+    int iterator = 0;
+    TrainStation previous;
+
+    public RandomTrains(long seed){
+        this.setSeed(seed);
+    }
+    public RandomTrains(){
+    }
+
+    public TrainStation nextTrainStation(){
+        TrainStation output = new TrainStation(iterator + " " + nextName());
+        try{
+            if(output.getLine().equals(previous.getLine())){
+                iterator++;}}catch(NullPointerException e){iterator++;}
+        previous = output;
+        return output;
+    }
+
+    public TrainLine nextTrainLine(){
+        TrainStation leftTerm = nextTrainStation();
+        TrainStation rightTerm = nextTrainStation();
+        leftTerm.setRight(rightTerm);
+        rightTerm.setLeft(leftTerm);
+        TrainLine output =  new TrainLine(leftTerm,rightTerm,nextName(),nextBoolean());
+        leftTerm.setTrainLine(output);
+        rightTerm.setTrainLine(output);
+        return output;
+    }
+
+    public void addConnectingStop(TrainLine lineA, TrainLine lineB){
+        String name = nextName();
+        TrainStation connectionA = new TrainStation(iterator + name + " 1 / 2");
+        TrainStation connectionB = new TrainStation(iterator + name + " 2 / 2");
+        connectionA.setConnection(lineB, connectionB);
+        connectionB.setConnection(lineA, connectionA);
+        lineA.addStation(connectionA);
+        lineB.addStation(connectionB);
+    }
+
+    public TrainNetwork nextTrainNetwork(){
+        int numLines = 2 + nextInt(23);
+        TrainNetwork output = new TrainNetwork(numLines);
+        TrainLine[] lines = new TrainLine[numLines];
+        for(int i = 0; i < numLines;i++){
+            lines[i] = nextTrainLine();
+            for(int j = 0; j < nextInt(10); j++){
+                lines[i].addStation(nextTrainStation());
+                if(nextInt(300) > 250 && i != 0){
+                    addConnectingStop(lines[i],lines[nextInt(i - 1)]);
+                }
+                if(i >= 1){//TODO: Make connecting stops add to random places always
+                    addConnectingStop(lines[i],lines[i-1]);
+                }
+            }
+
+
+        }
+        output.addLines(lines);
+        System.out.println("Created random train network with " + output.getLines().length + " lines");
+        return output;
+    }
+
+    public String nextName() {
+        int nameLength = nextInt(11);
+        nameLength += 1;
+        char[] nameOutChar = new char[nameLength];
+        for (int i = 0; i < nameLength; i++) {
+            if (nameLength <= 4) {
+                nameOutChar[i] = (char) nextInt(18500);
+                nameOutChar[i] += (char) nextInt(5000);
+            } else {
+                int nameNumber = nextInt(90 - 65) + 65;
+                nameOutChar[i] = (char) nameNumber;
+                if (i == nameLength - 3) {
+                    nameOutChar[i + 1] = (char) 85;
+                }
+            }
+        }
+        //System.out.println(nameOut);
+        return String.valueOf(nameOutChar);
+    }
+
+
+
+}
---
+++ src/COMP250A2_W2020/TrainVisualizer.java
@@ -0,0 +1,131 @@
+package COMP250A2_W2020;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+import static COMP250A2_W2020.TrainRide.generateTrainNetwork;
+
+public class TrainVisualizer extends JFrame {
+    public TrainNetwork tNet;
+    private Random rand = new Random();
+    public JFrame frame;
+    public TrainStation curStation;
+
+    public TrainVisualizer(TrainNetwork tNet){
+        frame = new JFrame("Network Visualizer V1 - Visit sashaphoto.ca");
+        setTitle("Network Visualizer V1");
+        setSize(800, 400);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.tNet = tNet;
+    }
+
+    public static void main(String[] args) {
+        TrainVisualizer t = new TrainVisualizer(generateTrainNetwork());
+        t.paint(null); // Not a proper way, but it still works.
+    }
+
+    public void paint(Graphics g) {
+        try{
+        g.clearRect(0,0,getWidth(),getHeight());}catch(NullPointerException e){}
+        TrainLine[] lines = tNet.networkLines;
+        TrainStation[][] linesStations = new TrainStation[lines.length][];
+        for (int i = 0; i < lines.length; i++) {
+            linesStations[i] = lines[i].getLineArray();
+        }
+        for (int i = 0; i < linesStations.length; i++) {
+            for (int j = 0; j < linesStations[i].length; j++) {
+                try {
+                    g.drawString(linesStations[i][j].getName(),
+                            (getWidth() * (j) + 1) / (linesStations[i].length + 1) + 15,
+                            getHeight() * (i + 1) / (linesStations.length + 1));
+
+                    g.drawRect((getWidth() * (j) + 1) / (linesStations[i].length + 1) + 15,
+                            getHeight() * (i + 1) / (linesStations.length + 1),
+                            15, 15);
+
+
+                    if (linesStations[i][j].hasConnection) {
+                        g.setColor(Color.gray);
+                        g.fillRect((getWidth() * (j) + 1) / (linesStations[i].length + 1) + 15,
+                                getHeight() * (i + 1) / (linesStations.length + 1),
+                                15, 15);
+                        g.setColor(Color.black);
+                        TrainLine transfersTo = linesStations[i][j].getTransferLine();
+                        TrainStation transfersStation = linesStations[i][j].getTransferStation();
+                        int coordYtrans = 0;
+                        for (int k = 0; k < lines.length; k++) {
+                            if (lines[k].getName().equals(transfersTo.getName())) {
+                                coordYtrans = k;
+//                                System.out.println("Y coord of transfer station of " + linesStations[i][j].getName() + " is " + coordYtrans);
+                            }
+                        }
+                        int coordXtrans = 0;
+                        TrainStation temp = lines[coordYtrans].getLeftTerminus();
+                        for (int k = 0; k < lines[coordYtrans].getSize(); k++) {
+                            if (temp.getName().equals(transfersStation.getName())) {
+                                coordXtrans = k;
+//                                System.out.println("X coord of transfer station of " + linesStations[i][j].getName() + " is " + coordXtrans);
+                            }
+                            temp = temp.getRight();
+                        }
+
+
+                        g.drawLine(
+                                //Location of original station
+                                (getWidth() * (j) + 1) / (linesStations[i].length + 1) + 15,
+                                getHeight() * (i + 1) / (linesStations.length + 1) + 8,
+                                //Location of transfer station
+                                (getWidth() * (coordXtrans) + 1) / (linesStations[coordYtrans].length + 1) + 15,
+                                getHeight() * (coordYtrans + 1) / (linesStations.length + 1) + 8);
+                    }
+                    if (linesStations[i][j].equals(curStation) || linesStations[i][j].getName().equals(curStation.getName())) {
+                        g.setColor(Color.cyan);
+                        g.fillRect((getWidth() * (j) + 1) / (linesStations[i].length + 1) + 15,
+                                getHeight() * (i + 1) / (linesStations.length + 1),
+                                15, 15);
+                        g.setColor(Color.black);
+                    }
+                    if (j != linesStations[i].length - 1) {
+                        g.drawLine((getWidth() * (j) + 1) / (linesStations[i].length + 1) + 15,
+                                getHeight() * (i + 1) / (linesStations.length + 1) + 8,
+                                (getWidth() * (j + 1) + 1) / (linesStations[i].length + 1) + 15,
+                                getHeight() * (i + 1) / (linesStations.length + 1) + 8);
+                    } else {
+                        g.drawString(tNet.getLines()[i].getName(),
+                                (getWidth() * (j + 1) + 2) / (linesStations[i].length + 1),
+                                getHeight() * (i + 1) / (linesStations.length + 1) + 8);
+                    }
+                } catch (NullPointerException e) {
+                }
+            }
+
+
+        }
+
+        //
+    }
+
+    public void delay(long pauseTimeMillis) {
+    /*This function pauses code execution for an input amount of time in milliseconds.
+    It exists only because delay() isn't a thing in java. */
+        try {
+            Thread.currentThread().sleep(pauseTimeMillis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void empty() {
+        invalidate();
+        validate();
+        repaint();
+        this.removeAll();
+
+    }
+
+    public void passCurrent(TrainStation curStation) {
+        this.curStation = curStation;
+    }
+}
