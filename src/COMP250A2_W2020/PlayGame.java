4b80fb33bbd2608662e0cafeaee40f0777dbd056
thebigsasha <sashathethird@gmail.com>
2020-02-24 01:44:57 -0500

    Improve game mode

--- src/COMP250A2_W2020/PlayGame.java
+++ src/COMP250A2_W2020/PlayGame.java
@@ -6,6 +6,7 @@ public class PlayGame {
         TrainLine[] lines = rand.nextTrainLineArray(5);
         GameRide game = new GameRide(lines.length);
         game.addLines(lines);
+        //TODO: implement levels & highscores
         game.gameTravel(lines[0].getRightTerminus().getLeft().getName(),
                 game.getLines()[0].getName(),
                 game.getLines()[game.getLines().length - 1].getRightTerminus().getName(),
